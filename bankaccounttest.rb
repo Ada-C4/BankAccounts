d03e5e996ccb7339a9417e98ceecde192229c172
Amy Hunter <amyrhunter@gmail.com>
2015-10-06 16:03:58 -0700

    Incomplete wave 1 commit with working withdraw and deposit methods.

---
+++ bankaccounttest.rb
@@ -0,0 +1,44 @@
+module Bank
+  class Account
+    attr_accessor :account_id, :account_balance, :amount
+
+    def initialize(account_id, account_balance = 0)
+      @account_id = account_id
+      @account_balance = account_balance.to_i
+      @amount = amount.to_i
+    end
+
+    def withdraw
+      puts "Type the amount you would like to withdraw:"
+      @amount = gets.chomp.to_i
+      @account_balance  -= @amount
+      puts "Your new balance is: #{@account_balance} dollars."
+    end
+
+    def deposit
+      puts "Type the amount you would like to deposit:"
+      @amount = gets.chomp.to_i
+      @account_balance += @amount
+      puts "Your new balance is #{@account_balance} dollars."
+    end
+
+  
+
+  end
+end
+#extra code
+
+
+
+# @default_account_amount = 0
+# puts "Your default account balance is $0. Please enter the amount you wish to deposit in whole dollars:"
+# @user_deposit  = gets.chomp
+# @integer_user_deposit = @user_deposit.to_i
+#   if @integer_user_deposit >= 0
+#     beginning_account_amount = @default_deposit_amount.to_i + @integer_user_deposit
+#     puts "You deposited #{@integer_user_deposit} dollars."
+#     puts "Your new balance is #{beginning_account_amount} dollars."
+#   else
+#     raise ArgumentError, "Please enter an amount that is greater than zero."
+#     @user_deposit = gets.chomp
+#   end
