56ca19116e70d244481a11fabb446d7a33479775
Rebecca Tolmach <rmtolmach@gmail.com>
2015-10-06 16:57:22 -0700

    Bank Account project with optional requirements

--- BankAccounts.rb
+++ BankAccounts.rb
@@ -1,10 +1,16 @@
 module Bank
   class Account
-    attr_reader :balance
+    attr_reader :balance # makes it so the user can only see their balance, not change it.
     def initialize(id, balance)
       @id = id
       @balance = balance
       raise ArgumentError.new("Permission Denied") if balance < 0
+    end
+
+# Once a new Owner is created, the variable holding the hash is passed in as 'person parameter'
+    def assign_owner(person)
+      @owner = person
+      puts "#{@owner.name} owns this account"
     end
 
     def withdrawl(amnt_withdrawn)
@@ -12,7 +18,7 @@ module Bank
         puts "You can't take out more than #{@balance}"
       else
       @balance = @balance - amnt_withdrawn # Return the updated account balance.
-      puts "Your current balance is: #{@balance}"
+      puts "Your current balance is: #{@balance}" # The updated balance
       end
     end
 
@@ -21,4 +27,22 @@ module Bank
       puts "Your current balance is: #{@balance}"
     end
   end
+
+# Owner sets up the code to accept the important info in a hash. Feed info in irb.
+  class Owner
+    attr_reader :name, :address, :phone_number, :email
+    def initialize(info)
+      @name = info[:name]
+      @address = info[:address]
+      @phone_number = info[:phone_number]
+      @email = info[:email]
+    end
+  end
 end
+
+info = {
+    name: "Jenna",
+    address: "2410 e pike",
+    phone_number: 1235230683,
+    email: "rmtolmach@gmail.com"
+}
