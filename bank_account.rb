fbeaa848bf683f03d7ab33d31f197264e625652e
Jennie Buechner <janina.buechner@gmail.com>
2015-10-06 16:19:50 -0700

    complete optional parts and test

--- bank_account.rb
+++ bank_account.rb
@@ -6,9 +6,10 @@ I18n.enforce_available_locales = false
 module Bank
 
   class Account
-    attr_reader :balance, :id
+    attr_reader :balance, :id, :owner
 
-    def initialize(balance = 0)
+    def initialize(balance = 0, owner = nil)
+      @owner = owner
       # Creates an ID of random numbers
       @id = ('0'..'9').to_a.shuffle[0..8].join
       if balance < 0
@@ -43,11 +44,29 @@ module Bank
       puts "The current balance of this account is " + money.format
     end
 
+    def formatted_name
+      return @owner.first_name + " " + @owner.last_name
+    end
+
+    def assign_owner(owner)
+      @owner = owner
+      puts "The new owner of account ##{id} is #{formatted_name}"
+    end
+
   end
 
   class Owner
-    def initialize
+    attr_reader :first_name, :last_name, :address1, :address2, :city, :state, :zip
+    def initialize(person_hash)
+      @first_name = person_hash[:first_name]
+      @last_name = person_hash[:last_name]
+      @address1 = person_hash[:address1]
+      @address2 = person_hash[:address2]
+      @city = person_hash[:city]
+      @state = person_hash[:state]
+      @zip = person_hash[:zip]
     end
+
   end
 
 end
