3c5a335e0f0cf85eaa825991c552bd7851584dbb
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-06 14:57:18 -0700

    first pass at optional requirements

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -4,12 +4,17 @@
 module Bank
 
   class Account
-    attr_reader :balance
+    attr_reader :balance, :owner
 
     def initialize(ident, balance = 0)  # optional parameter
       @ident = ident
       @balance = balance
       raise ArgumentError.new("Your intial balance can't be negative") if balance < 0
+    end
+
+    def assign_owner(person) # pass in an instance of Owner
+      @owner = person
+      puts "The other of the account is #{@owner.first_name} #{@owner.last_name}"
     end
 
     def withdraw(withdraw_amount)
@@ -26,4 +31,35 @@ module Bank
     end
   end
 
+  class Owner
+    attr_reader :first_name, :last_name, :address_one, :address_two, :city, :state, :zip
+
+    def initialize(owner_hash)
+      @first_name = owner_hash[:first_name]
+      @last_name = owner_hash[:last_name]
+      @address_one = owner_hash[:address_one]
+      @address_two = owner_hash[:address_two]
+      @city = owner_hash[:city]
+      @state = owner_hash[:state]
+      @zip = owner_hash[:zip]
+    end
+
+  end
+
 end
+
+JENNA_ACCT = Bank::Account.new("Jenna ID", 1200)
+
+jenna_info = {
+  first_name: "Jenna",
+  last_name: "Nichols",
+  address_one: "4900 Linden Ave N",
+  city: "Seattle",
+  state: "WA",
+  zip: 98103
+}
+
+JENNA_OWNER = Bank::Owner.new(jenna_info)
+
+# can assign JENNA_OWNER to JENNA_ACCT as follows
+# JENNA_ACCT.assign_owner(JENNA_OWNER)
