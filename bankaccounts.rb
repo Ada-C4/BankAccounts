131688e52204820042359c2cb001f9852c468d0f
Desiree <desireepoland@gmail.com>
2015-10-06 16:17:13 -0700

    Optional enhancements added

--- bankaccounts.rb
+++ bankaccounts.rb
@@ -1,21 +1,23 @@
 module Bank
   class Account
-    def initialize(initial_balance)
+    attr_reader :balance, :owner, :id
+    def initialize(initial_balance, owner = nil)
       @balance = check_initial_balance(initial_balance)
       @id = rand(99999)
+      @owner = owner
     end
 
     def check_initial_balance(initial_balance)
       if initial_balance < 0
-        raise ArgumentError, "Can not add a negative initial balance."
+        raise ArgumentError, "Invalid Balance: Can not add a negative balance."
       else
         return initial_balance
       end
     end
 
     def withdraw(withdraw_amount)
-      if @balance - withdraw_amount < 0
-        puts "Warning: Can not withdraw more than is account."
+      if withdraw_amount > @balance
+        puts "Warning: Can not withdraw more than is account. Transaction terminated."
         return @balance
       else
         @balance -= withdraw_amount
@@ -28,8 +30,20 @@ module Bank
       return @balance
     end
 
-    def check_balance
-      puts @balance
+    def add_owner(owner_name)
+      @owner = owner_name
+    end
+
+  end
+
+  class Owner
+    attr_reader :id, :firstname, :lastname, :birthdate, :address
+    def initialize(owner_hash)
+      @id = rand(99999)
+      @firstname = owner_hash[:firstname]
+      @lastname = owner_hash[:lastname]
+      @birthdate = owner_hash[:birthdate]
+      @address = owner_hash[:address]
     end
   end
 
@@ -38,6 +52,8 @@ end
 
 
 
-
-
-#a = Bank::Account.new(50000)
+# a = Bank::Account.new(50000)
+# chandler_hash = { firstname: "Chandler", lastname: "Bing", birthdate: "4/8/68", address: "NYC" }
+# chandler = Bank::Owner.new(chandler_hash)
+# joey = Bank::Owner.new({firstname: "Joey", lastname: "Tribbiani", birthdate: "1/9/68", address: "NYC" })
+# a.add_owner(chandler)
