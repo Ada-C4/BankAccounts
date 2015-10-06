b1395a2701111e036973d4256faf2fa40d2578a1
Rebecca Tolmach <rmtolmach@gmail.com>
2015-10-06 14:49:09 -0700

    Primary requirements

--- BankAccounts.rb
+++ BankAccounts.rb
@@ -1,19 +1,24 @@
 module Bank
   class Account
     attr_reader :balance
-    def initialize(balance)
+    def initialize(id, balance)
+      @id = id
       @balance = balance
       raise ArgumentError.new("Permission Denied") if balance < 0
     end
 
     def withdrawl(amnt_withdrawn)
-      # Return the updated account balance.
-      # Raise a warning message if user tries to withdrawl more than they have.
+      if amnt_withdrawn > @balance
+        puts "You can't take out more than #{@balance}"
+      else
+      @balance = @balance - amnt_withdrawn # Return the updated account balance.
+      puts "Your current balance is: #{@balance}"
+      end
     end
 
     def deposit(amnt_deposited)
-      #  Return the updated account balance.
+      @balance = @balance + amnt_deposited #Return the updated account balance.
+      puts "Your current balance is: #{@balance}"
     end
-
   end
 end
