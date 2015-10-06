022ed2c3256d89e30aa407110900b76f7a97fd58
Claire <ClaireS29@gmail.com>
2015-10-06 16:48:01 -0700

    final version

--- bank_account.rb
+++ bank_account.rb
@@ -2,9 +2,10 @@ module Bank
 
   class Account
     attr_reader :balance, :id, :owner
-    def initialize(owner_hash, balance = 0, id = 7383092)
+    def initialize(balance = 0, id = 7383092, owner = nil)
       @balance = balance
       @id = 7383092
+      @owner = owner
       if balance < 0
         raise ArgumentError, "Accounts cannot be opened with negative money"
       end
@@ -29,23 +30,24 @@ module Bank
       puts "You balance is #{@balance} cents"
     end
 
-    def check_owner_info
-
+    def add_owner(owner)
+      @owner = owner
     end
   end
 
   class Owner
-    attr_reader :account, :first_name, :last_name, :address, :email
-    def initialize(account, owner_hash)
+    attr_reader :account, :first_name, :last_name, :address, :email, :owner_id
+    def initialize(owner_hash)
       @account = account
       @first_name = owner_hash[:first_name]
       @last_name = owner_hash[:last_name]
       @address = owner_hash[:address]
       @email = owner_hash[:email]
+      @owner_id = owner_hash[:id]
     end
 
     def who_da_owna
-      puts "This account is owned by #{@first_name} #{@last_name}"
+      puts "This account is owned by #{@first_name} #{@last_name} with #{@owner_id}"
     end
   end
 end
@@ -54,15 +56,14 @@ owner_hash = {
 first_name: "Winnie",
 last_name: "The Pooh",
 address: "Deep in the Hundred Acre Woods",
-email: "poohbear@christopherrobin.com"
+email: "poohbear@christopherrobin.com",
+id: 2376423
 }
 
-a = Bank::Account.new(owner_hash)
-o = Bank::Owner.new(a, owner_hash)
-
+a = Bank::Account.new
+o = Bank::Owner.new(owner_hash)
+a.add_owner(o)
+puts "#{a.owner.first_name}"
 a.deposit(100)
-a.check_balance
 a.withdraw(50)
-a.check_balance
 a.withdraw(70)
-o.who_da_owna
