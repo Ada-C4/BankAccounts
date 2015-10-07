be423621c2d6dd1221fbb8302f6aa1fe9fdeaf31
Tammy <misstamar@gmail.com>
2015-10-06 23:06:00 -0700

    both classes in 1 file

--- bank.rb
+++ bank.rb
@@ -1,54 +1,83 @@
+require "./Owner.rb"
 module Bank
+  class Account
 
-class Account
+    attr_accessor :balance, :id, :owner
 
-attr_accessor :balance, :id
+    def initialize (owner)
+      @balance = initial_balance
+      @id = rand(200)
+      @owner = owner
+    end
 
-def initialize
-  @balance = initial_balance
-  @id = 124
-end
 
+    def initial_balance
+      puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(70)
+      puts "$$$$$$$ Welcome to Tammy's Bank! $$$$$$$".center(70)
+      puts "How much $$$ would you like to deposit to your new account ?".center(70)
+      puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".center(70)
+      money = gets.chomp.to_i
+      if money < 0
+        raise ArgumentError.new("You can't deposit negativ amount of money")
+        money = 0
+      elsif money == 0
+        puts "You have 30 days to deposit at least $5"
+      end
+      return money
+    end
 
-def initial_balance
-  puts "Welcome to Ada Bank!"
-  puts "How much money would you like to deposit to your new account ?"
-  money = gets.chomp.to_i
-  if money < 0
-    raise ArgumentError.new("You can't deposit negativ amount of money")
-    retuern 0
-  elsif
-    money == 0
-    puts "You have 30 days to deposit at least $5"
+    def withdraw (amount)
+      if amount > @balance
+        puts "The maximun amount you can draw is #{@balance} pennies"
+      else
+        puts "You had $#{@balance/100}"
+        @balance = @balance - amount
+        puts "The update account balance is $#{@balance/100}"
+      end
+    end
+
+    def deposit (amount)
+      if amount < 0
+        puts "If you want to withdraw, that's a different method :)"
+      else
+        @balance = @balance + amount
+        puts "After you deposited $#{amount/100}"
+        puts "The update account balance is $#{@balance/100}"
+      end
+    end
+
+    def print_balance
+      puts "Your current balance is $#{@balance}"
+    end
+
   end
-  return money
-end
+
+  class Owner
+
+    attr_accessor :name, :address, :job, :accounts, :id
+
+    def initialize (info_hash)
+      @name = info_hash [:name]
+      @address = info_hash [:address]
+      @job = info_hash [:job]
+      @accounts = []
+      @id = info_hash[:id]
+    end
+
+    def add_account
+      account = Bank::Account.new(self)
+      @accounts.push(account)
+    end
 
 
-def withdraw (amount)
-  if amount > @balance
-    puts "The maximun amount you can draw is $#{@balance}"
-  else
-  puts "You had $#{@balance}"
-  @balance = @balance - amount
-  puts "The update account balance is $#{@balance/100}"
+  nemo_hash = {
+  name: "Nemo",
+  address: "Seattle",
+  job: "Student",
+  id: 1283
+  }
+
+  nemo = Bank::Owner.new(nemo_hash)
+
   end
-end
-
-def deposit (amount)
-  if amount < 0
-    puts "If you want to withdraw, that's a different method :)"
-  else
-  @balance = @balance + amount
-  puts "After you deposited $#{amount/100}"
-  puts "The update account balance is $#{@balance/100}"
-  end
-end
-
-def print_balance
-  puts "Your current balance is $#{@balance}"
-end
-
-
-end
 end
