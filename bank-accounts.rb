8bcf2452c759a8c9cacb1c45ed0c83919c290b74
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-07 14:59:43 -0700

    finished first pass at primary reqs for Wave 2

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -1,15 +1,33 @@
 # run this file with
 # irb -r "./bank-accounts.rb"
 
+require 'csv'
+
 module Bank
 
   class Account
-    attr_reader :balance, :owner
+    attr_reader :balance, :owner, :open_date, :ident
 
-    def initialize(ident, balance = 0)  # optional parameter
+    def initialize(ident, balance, open_date)
       @ident = ident
       @balance = balance
       raise ArgumentError.new("Your intial balance can't be negative") if balance < 0
+      @open_date = DateTime.strptime(open_date, '%Y-%m-%d %H:%M:%S %z')
+    end
+
+    def self.all
+      account_array = []
+      CSV.read("./support/accounts.csv", 'r').each do |row|
+        account = Bank::Account.new(row[0].to_i, row[1].to_i, row[2])
+        account_array.push(account)
+      end
+      return account_array
+    end
+
+    def self.find(id)
+      self.all.find do |account|
+        account.ident == id
+      end
     end
 
     def assign_owner(person) # pass in an instance of Owner
@@ -52,18 +70,18 @@ end
 ### example ###
 ### constants are used for availability in irb ###
 
-JENNA_ACCT = Bank::Account.new("Jenna ID", 1200)
-
-jenna_info = {
-  first_name: "Jenna",
-  last_name: "Nichols",
-  address_one: "4900 Linden Ave N",
-  city: "Seattle",
-  state: "WA",
-  zip: "98103"
-}
-
-JENNA_OWNER = Bank::Owner.new(jenna_info)
+# JENNA_ACCT = Bank::Account.new("Jenna ID", 1200)
+#
+# jenna_info = {
+#   first_name: "Jenna",
+#   last_name: "Nichols",
+#   address_one: "4900 Linden Ave N",
+#   city: "Seattle",
+#   state: "WA",
+#   zip: "98103"
+# }
+#
+# JENNA_OWNER = Bank::Owner.new(jenna_info)
 
 # can assign JENNA_OWNER to JENNA_ACCT as follows
 # JENNA_ACCT.assign_owner(JENNA_OWNER)
