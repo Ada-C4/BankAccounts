f6dd3e88bde88740854fca45e3bfc56b7ca4980e
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-08 15:50:54 -0700

    completed primary requirements for CheckingAccount class

--- checking-account.rb
+++ checking-account.rb
@@ -1,5 +1,34 @@
 module Bank
-  class CheckingAccount
+  class CheckingAccount < Account
+    attr_reader :used_checks
+
+    def initialize(ident, open_date, balance)
+      super
+      @used_checks = 0
+    end
+
+    def withdraw(withdraw_amount)
+      super(withdraw_amount + 100)
+    end
+
+    def withdraw_using_check(amount)
+      if @balance - amount < -1000
+        puts "Insufficient new balance. Check denied."
+        @balance
+      else
+        @used_checks += 1
+        if @used_checks > 3
+          @balance = @balance - amount - 200
+        else
+          @balance -= amount
+        end
+      end
+    end
+
+    def reset_checks
+      @used_checks = 0
+    end
+
 
   end
 
