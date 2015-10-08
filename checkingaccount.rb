7a9d54bc4bccae7e069343a1adcd9b6c92029b3f
Ricky Hougland <houglande@gmail.com>
2015-10-08 16:28:51 -0700

    finished primary requirements for W3

--- checkingaccount.rb
+++ checkingaccount.rb
@@ -1,10 +1,13 @@
 module Bank
 
   class CheckingAccount < Account
+    attr_reader :free_checks
+    
     def initialize(id, balance, open_date)
       super
 
       @act_withdraw_fee = 100
+      @free_checks = 3
     end
 
     def withdraw(withdraw_amt)
@@ -14,18 +17,21 @@ module Bank
     def withdraw_using_check(amount)
       withdraw_to_cents = amount * 100
 
-      free_checks = 3
-
-      if (@balance - withdraw_to_cents) < (@min_balance + 1000)
+      if (@balance - withdraw_to_cents) < (@min_balance - 1000)
         puts "You cannot withdraw that much. Your current balance is #{@balance} cents"
-      elsif free_checks > 0
-        @balance -= (withdraw_to_cents)
+      elsif @free_checks > 0
+        @balance -= withdraw_to_cents
+        @free_checks -= 1
         puts "Your account balance is now #{@balance} cents."
-        free_checks -= 1
       else
-
+        @balance -= (withdraw_to_cents - 200)
+        puts "You already used up your free checks for this month, so you've been charged an extra $2."
+        puts "Your new balance is #{@balance} cents."
       end
+    end
 
+    def reset_checks
+      @free_checks = 3
     end
 
 
