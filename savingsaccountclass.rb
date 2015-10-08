e0a6e4b845e0884acf9c8592ce514951aefedd99
Amy Hunter <amyrhunter@gmail.com>
2015-10-08 16:40:49 -0700

    First commit of savings account class.

--- savingsaccountclass.rb
+++ savingsaccountclass.rb
@@ -1,6 +1,8 @@
 require "./bankrequirefiles.rb"
+require "./bankaccounttest.rb"
 module Bank
   class SavingsAccount < Account
+    attr_accessor = :amount, :balance
     def initialize(balance)
       @balance = balance
       if @balance < 1000
@@ -8,7 +10,23 @@ module Bank
       end
     end
 
-    # def add_interest(rate)
-    # end
+    def savings_withdraw(withdraw)
+      withdrawal_amount = withdraw + 200
+      if @balance - withdrawal_amount <= 1000
+        puts "The account minimum balance is $10 (1000 cents). You cannot go below that minimum."
+        puts "Your current balance is #{@balance}"
+      else
+        @balance -= withdrawal_amount
+      end
+    end
+
+    def add_interest(rate)
+      account_interest = @balance * rate/100
+      @balance += account_interest
+      puts account_interest
+      print @balance
+    end
+
+
   end
 end
