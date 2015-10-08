60e0c11999f5432181e8a3038f2477476e225b2a
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-07 19:12:29 -0700

    Wave 2 program, with all elements functioning

--- bank.rb
+++ bank.rb
@@ -1,23 +1,45 @@
+require 'csv'
+# require "./support/accounts.csv"
+
 module Bank
 
   class Account
     attr_reader :balance, :owner, :account_id, :owner_id
 
-    def initialize (id, initial_balance)
+    def initialize (id, initial_balance, open_date)
       @account_id = id
       raise ArgumentError.new("You can't start an account with a negative balance.") if initial_balance <= 0
-      @balance = initial_balance
-      # Initial balance cannot be negative, raises an Argument Errors
+      @balance = initial_balance # in cents
+      @open_date = DateTime.strptime(open_date, "%Y-%m-%d %H:%M:%S %z")
     end
 
-    # Withdraw money
-    # Make sure account doesn't fall into the negative because of withdrawal
-
-    # Pass in an account owner from an already created Owner instance
-    def add_owner(owner)
-      @owner = owner
+    # Return an array of all Account instances in account.csv
+    def self.all
+      account_array = []
+      CSV.read("./support/accounts.csv").each do |row|
+        account = Bank::Account.new(row[0].to_i, row[1].to_i, row[2])
+        account_array.push(account)
+      end
+      return account_array
     end
 
+    # returns an instance of Account where the value of the id field in the CSV matches the passed parameter
+    def self.find(id)
+      self.all.find {|account| account.account_id == id}
+    end
+
+    def self.account_owners(account_id)
+      account = self.all.find{|account| account.account_id == account_id}
+      match_array = []
+      CSV.read("./support/account_owners.csv").each do |row|
+        array = [row[0].to_i, row[1].to_i]
+        match_array.push(array)
+      end
+      match = match_array.find{|set| set[0] == account.account_id}
+      @owner = Bank::Owner.all.find{|owner| match[1] == owner.owner_id}
+    end
+
+    # Withdraw money. Cannot withdraw to make account negative.
     def withdraw(amount)
       new_balance = @balance - amount
       if amount < 0
@@ -45,16 +67,29 @@ module Bank
   end
 
   class Owner
-    attr_reader :first_name, :last_name, :street_address, :city, :state
+    attr_reader :first_name, :last_name, :street_address, :city, :state, :owner_id
 
-    def initialize(owner_id, owner_hash)
+    def initialize(owner_id, last_name, first_name, street_address, city, state)
       @owner_id = owner_id
-      @account_id_1 = owner_hash[:account_id]
-      @first_name = owner_hash[:first_name]
-      @last_name = owner_hash[:last_name]
-      @street_address = owner_hash[:street_address]
-      @city = owner_hash[:city]
-      @state = owner_hash[:state]
+      @last_name = last_name
+      @first_name = first_name
+      @street_address = street_address
+      @city = city
+      @state = state
+    end
+
+    # returns a collection of Owner instances, representing all of the Owners described in the CSV.
+    def self.all
+      owner_array = []
+      CSV.read("./support/owners.csv").each do |row|
+        owner = Bank::Owner.new(row[0].to_i, row[1], row[2], row[3], row[4], row[5])
+        owner_array.push(owner)
+      end
+      return owner_array
+    end
+
+    def self.find(owner_id)
+      self.all.find {|owner| owner.owner_id == owner_id }
     end
 
   end
