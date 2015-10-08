01b86685cbf71a5cd7a262030250dba16bc0865a
Claire <ClaireS29@gmail.com>
2015-10-08 15:14:08 -0700

    Added Checking Account Skeleton

--- lib/savings_and_checking.rb
+++ lib/savings_and_checking.rb
@@ -18,29 +18,43 @@ module Bank
     end
 
     def add_interest(rate)
-      interest_rate = rate
-      added_rate = @balance * (interest_rate/100)
+      interest_rate = rate.to_f
+      added_rate = @balance * (interest_rate/100.to_f)
       @balance = @balance + added_rate
       return added_rate
     end
-
-
-
-
-
-
-
-
-
-
   end
 
-
-
-
-
-
-
   class CheckingAccount < Account
+    def initialize(id, balance, open_date)
+      #super(id, balance, open_date)
+      @check_count = 0
+
+    end
+
+    def withdraw(withdraw_amount)
+      transaction_fee = 100
+      @balance = @balance.to_i - transaction_fee.to_i - withdraw_amount.to_i
+      if @balance < 0
+        puts "You cannot have less than $0. This withdraw has been cancelled."
+        @balance = @balance + withdraw_amount.to_i + transaction_fee
+      end
+      return @balance
+    end
+
+    def withdraw_using_check(amount)
+      @balance = @balance.to_i - amount.to_i
+      @check_count += 1
+        if @check_count > 3
+          puts "You do not have any more checks this month. Transaction cancelled."
+          @balance = @balance.to_i + amount.to_i
+        end
+        return @balance
+    end
+
+    def reset_checks
+      
+
+    end
   end
 end
