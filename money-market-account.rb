bc3fd3fcc404825c50c2bfc32965c086becc9fb7
Daphne <dg1181@nyu.edu>
2015-10-08 16:23:15 -0700

    MoneyMarketAccount lock for balance < 1000000

--- money-market-account.rb
+++ money-market-account.rb
@@ -5,25 +5,35 @@ module Bank
       super
 
       @transactions = 0
+      @lock = false
+      raise ArgumentError if @balance < 1000000
     end
 
     def withdraw(withdrawal)
-      if @transactions < 6
+      if @transactions < 6 && @lock == false
         super
         @transactions += 1 if withdrawal <= @balance
+        if @balance < 1000000
+          @balance -= 10000
+          @lock = true
+        end
       else
-        print "You have reached your maximum of 6 transactions this month. "
+        print "Your account is locked until your balance is restored to 1000000. " if @lock
+        print "You have reached your maximum of 6 transactions this month. " if @lock == false
       end
 
       return @balance
     end
 
     def deposit(deposit_amt)
-      if @transactions < 6
+      @lock = false if deposit_amt + @balance > 1000000
+
+      if @transactions < 6 && @lock == false
         super
         @transactions += 1 if deposit_amt <= @balance
       else
-        print "You have reached your maximum of 6 transactions this month. "
+        print "Your account is locked until your balance is restored to 1000000. " if @lock
+        print "You have reached your maximum of 6 transactions this month. " if @lock == false
       end
 
       return @balance
