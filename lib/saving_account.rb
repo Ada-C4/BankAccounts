b84d1c7b30f38fdc5ea5f7804c815d437b5f9c10
Tammy <misstamar@gmail.com>
2015-10-08 16:16:42 -0700

    yay

--- lib/saving_account.rb
+++ lib/saving_account.rb
@@ -1,25 +1,27 @@
 module Bank
-class SavingsAccount < Account
+  require "./account.rb"
+  class SavingsAccount < Account
 
-  def initialize (id, balance, date)
-    super (id, initial_balance, date)
+
+    def initialize (id, balance, date)
+      super
+    end
+
+    def initial_balance
+      super(1000)
+    end
+
+
+    def withdraw (amount)
+      super(amount, 200, 1000) #in cents
+    end
+
+
+    def add_interest(rate)
+      interest = @balance * rate
+      @balance = @balance + interest
+      return interest
+    end
+
   end
-
-  def initial_balance
-    super (1)
-  end
-
-
-  def withdraw (amount)
-    super (amount, 2, 10)
-  end
-
-  def dd_interest(rate)
-    interest = @balance * rate
-    @balance = @balance + interest
-    return interest
-  end
-
-
-
 end
