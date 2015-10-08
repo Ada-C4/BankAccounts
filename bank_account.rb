ea37d81f6f571fd5bdd046e56a26b048cc072fca
Annalee <annaleeherrera@gmail.com>
2015-10-08 14:58:24 -0700

    Modifying Bank Account Wave 1

--- bank_account.rb
+++ bank_account.rb
@@ -1,33 +1,33 @@
 module Bank
   class Account
-    attr_accessor :name, :initial_balance
-
+    attr_accessor :name, :balance
     def initialize (name, initial_balance)
-    @name = name
-    @initial_balance= initial_balance
-    puts "Welcome, #{name}.  You have #{initial_balance} in your account."
-    end
-
-    def new_account (initial_balance)
-      if @initial_balance < 0
+      @name = name
+      @balance= initial_balance
+      if @balance < 0
         raise ArgumentError.new ("You must start an account with more funds than $0")
-      puts "Welcome, #{@name}.  Thank you for opening a new account."
+      else
+      puts "Welcome, #{@name}.  Thank you for opening a new account. Your balance is $#{@balance}"
       end
     end
 
     def withdrawal (withdrawal_amount)
       if withdrawal_amount > @balance
-      puts "You have insufficient funds. You have #{@balance} in your account."
+      puts "You have insufficient funds. You have $#{@balance} in your account."
     elsif withdrawal_amount > 0
-      @balance -= withdrawal_amount
-      puts "You withdrew #{withdrawal_amount}.  Your new balance is #{@balance}."
+      @balance = @balance - withdrawal_amount
+      puts "You withdrew $#{withdrawal_amount}.  Your new balance is #{@balance}."
       end
+    end
+
+    def balance
+      puts @balance
     end
 
     def deposit (deposit_amount)
       if deposit_amount > 0
-        @balance += deposit_amount = new_balance
-        puts "You deposited $#{deposit_amount}.  Your new balance is #{new_balance}."
+        @balance = @balance + deposit_amount
+        puts "You deposited $#{deposit_amount}.  Your new balance is #{@balance}."
       end
     end
   end
