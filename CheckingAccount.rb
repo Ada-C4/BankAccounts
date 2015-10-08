8e5508ce83cfbaf3263bbb2c1250d7eee389f916
emgord <emgord@gmail.com>
2015-10-08 16:02:15 -0700

    Added withdraw with check to checking account class

--- CheckingAccount.rb
+++ CheckingAccount.rb
@@ -1,11 +1,32 @@
 module Bank
 
-class CheckingAccount < Account
+  class CheckingAccount < Account
+
+    attr_reader :free_check
+
+    def initialize(account_id, initial_balance, open_date, min_balance = 0, fee = 100)
+      super(account_id, initial_balance, open_date, min_balance, fee)
+      @free_check = 3
+    end
+
+    def withdraw_using_check(amount)
+      if @free_check > 0
+        check_fee = 0
+      else
+        check_fee = 200
+      end
+
+      if amount <= (@balance + 1000)
+          @balance -= (amount + check_fee)
+          @free_check -= 1
+      else
+        puts "Your account cannot overdraft by more than 1000. Your balance is #{@balance} so you cannot write a check for #{amount} at this time."
+      end
+      return @balance
+    end
 
 
 
-
-
-end
+  end
 
 end
