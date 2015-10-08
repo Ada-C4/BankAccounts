6d353aeb3bbf82d6addcfc84db45f4039c2044d0
Jennie Buechner <janina.buechner@gmail.com>
2015-10-08 15:39:09 -0700

    begin checking account class and reset checks

--- banktest.rb
+++ banktest.rb
@@ -2,19 +2,23 @@ require './lib/savings'
 require './lib/checking'
 
 # - Normal Account Tests -
-account = Bank::Account.new(1, 100)
-account.print_balance
-account.withdraw(20) # Regular withdrawal
-account.withdraw(1100) # Withdrawing too much
+# account = Bank::Account.new(1, 100)
+# account.print_balance
+# account.withdraw(20) # Regular withdrawal
+# account.withdraw(1100) # Withdrawing too much
 
 # account = Bank::Account.new(1, -10) # Account below min
 
 # - Savings Account Tests -
-savings = Bank::SavingsAccount.new(1, 100000)
-savings.print_balance
-savings.withdraw(20000) # Regular withdrawal
-savings.withdraw(1000000000) # Withdrawing too much
-
-savings = Bank::SavingsAccount.new(1, 10) # Account below min
+# savings = Bank::SavingsAccount.new(1, 100000)
+# savings.print_balance
+# savings.withdraw(20000) # Regular withdrawal
+# savings.withdraw(1000000000) # Withdrawing too much
+#
+# savings = Bank::SavingsAccount.new(1, 10) # Account below min
 
 # savings.add_interest(1)
+
+# - Checking Account Tests -
+checking = Bank::CheckingAccount.new(1, -100)
+checking.print_balance
--- lib/checking.rb
+++ lib/checking.rb
@@ -4,16 +4,17 @@ module Bank
   class CheckingAccount < Account
     MAX_CHECKS = 3
     FEE = 100
-    def initialize
+    def initialize(id, balance, open_date = "today", owner = nil)
+      super(id, balance, open_date, owner)
       @type = "Checking"
-      # Create a minimum balance for this type of account
-      @min_balance = 0
       @fee = 100
       @num_checks = 0
     end
 
+    def reset_checks
+      puts "It's the dawn of a new month and your checks are back to 0!"
+      @num_checks = 0
+    end
 
   end
-
-
 end
