5b06c7f5fc3d7901c3333db0ae47fde94d5cf91f
emgord <emgord@gmail.com>
2015-10-08 15:30:07 -0700

    added interest rate to savings account

--- SavingsAccount.rb
+++ SavingsAccount.rb
@@ -2,14 +2,19 @@
 
 module Bank
 
-class SavingsAccount < Account
+  class SavingsAccount < Account
 
-def initialize(account_id, initial_balance, open_date, min_balance = 1000, fee = 200)
-  super(account_id, initial_balance, open_date, min_balance, fee)
-end
+      def initialize(account_id, initial_balance, open_date, min_balance = 1000, fee = 200)
+        super(account_id, initial_balance, open_date, min_balance, fee)
+      end
+
+      def add_interest(rate)
+      interest = @balance * (rate/100)
+      @balance += interest
+      return interest
+      end
 
 
-
-end
+  end
 
 end
