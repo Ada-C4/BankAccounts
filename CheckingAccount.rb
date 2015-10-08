ad5ef2ac1e2f7ed2c25965dc1bb7ac262493555a
Brittany <BrittanyKohler@gmail.com>
2015-10-08 15:54:30 -0700

    Updating withdraw_using_check method to count number of checks used, adding reset_checks method

--- CheckingAccount.rb
+++ CheckingAccount.rb
@@ -11,13 +11,21 @@ module Bank
       end
 
       def withdraw_using_check(amount)
-        min_balance = -10
-        if (@balance - amount < -1000)
-          puts "Your balance cannot be below the minimum amount of $#{min_balance}. Transaction terminated."
+        min_balance = -1000
+        if @num_checks_used >= 3
+          amount += 200
+        end
+        if (@balance - amount < min_balance)
+          puts "Your balance cannot be below the minimum amount of $#{min_balance/100}. Transaction terminated."
         else
+          @num_checks_used += 1
           @balance -= amount
         end
         return @balance
+      end
+
+      def reset_checks
+        @num_checks_used = 0
       end
   end
 end
