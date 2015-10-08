865159298b4dd26dcb6a164b3130b0c76a98e2c4
Daphne <dg1181@nyu.edu>
2015-10-08 14:22:21 -0700

    Added withdraw functionality to SavingsAccount class

--- savings-account.rb
+++ savings-account.rb
@@ -4,9 +4,19 @@ module Bank
     def initialize(id, initial_balance)
       @id = id.to_i
       @balance = initial_balance.to_i
-      
+
       raise ArgumentError if @balance < 1000
     end
 
+    def withdraw(withdrawal)
+      if @balance - withdrawal <= 1000
+        puts "Your account balance cannot drop below $10."
+      else
+        super
+        @balance -= 200
+      end
+
+      return @balance
+    end
   end
 end
