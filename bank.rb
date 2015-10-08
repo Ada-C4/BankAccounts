5e3b7da2b3d9c99048dfd51b7585bac208e9838f
Desiree <desireepoland@gmail.com>
2015-10-08 15:52:31 -0700

    Update withdraw and add withdraw_using_check and reset_checks methods in CheckingAccount Class

--- bank.rb
+++ bank.rb
@@ -4,6 +4,9 @@ require "./savings.rb"
 require "./checking.rb"
 
 
-# Savings Account
+# Savings Account tests
 # s = Bank::SavingsAccount.new("1212","10000","1999-03-27 11:30:09 -0800")
-# d = Bank::SavingsAccount.new("4444","99999999","1999-03-27 11:30:09 -0800")
+# s2 = Bank::SavingsAccount.new("4444","99999999","1999-03-27 11:30:09 -0800")
+
+# Checking Account tests
+# c = Bank::CheckingAccount.new("5555","10000","1999-03-27 11:30:09 -0800")
--- checking.rb
+++ checking.rb
@@ -1,7 +1,49 @@
 
 module Bank
-
   class CheckingAccount < Account
-  end
+    attr_reader :checks_used
 
+    def initialize(id, initial_balance, open_date)
+      super(id, initial_balance, open_date)
+      @checks_used = 0
+    end
+
+    def withdraw(withdraw_amount)
+      fee = 100
+      if withdraw_amount > (@balance + 100)
+        puts "Warning: Can not withdraw more than is in account. Transaction terminated."
+        return @balance
+      else
+        @balance -= (withdraw_amount + fee)
+        puts "There is a transaction fee of $1.00."
+        return @balance
+      end
+    end
+
+    def withdraw_using_check(withdraw_amount)
+      fee = 200
+      if withdraw_amount > (@balance + 1000)
+        puts "Warning: Can not overdraft account more than $10.00. Transaction terminated."
+        return @balance
+      else
+        if @checks_used < 3
+          @balance -= withdraw_amount
+          @checks_used += 1
+          return @balance
+        else
+          @balance -= (withdraw_amount + fee)
+          puts "You have used up your 3 free check withdrawals this month."
+          puts "There is a transaction fee of $2.00"
+          @checks_used += 1
+          return @balance
+        end
+      end
+    end
+
+    def reset_checks
+      @checks_used = 0
+    end
+
+
+  end
 end
