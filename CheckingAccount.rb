43f909a75bef89f6b23ecb191f6fc45fe5549842
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-08 16:34:12 -0700

    updated withdraw check method, added reset_check method

--- CheckingAccount.rb
+++ CheckingAccount.rb
@@ -1,16 +1,31 @@
 require './BankAccounts.rb'
 class CheckingAccount < Bank::Account
+  attr_reader :check_count
+  def initialize(ident, balance, open_date)
+    super
+    @check_count = 0
+    @check_fee = 100
+  end
   def withdraw(amount)
     amount += 100
     super
   end
   def withdraw_using_check(amount)
-    amount += 100
     if amount > (@balance + 1000)
       puts "Not enough fundz!"
     else
-      @balance -= amount
+      if @check_count < 3
+        @check_fee = 100
+        @balance -= (amount + @check_fee)
+        @check_count += 1
+      else
+        @check_fee = 300
+        @balance -= (amount + @check_fee)
+      end
+      return @balance
     end
-    return @balance
+  end
+  def reset_checks
+    @check_count = 0
   end
 end
