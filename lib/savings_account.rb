0073289650e8532937802f1ad0e16ee4e8c497cd
noglows <jessica.noglows@outlook.com>
2015-10-09 16:19:01 -0700

    Added comments to savings account

--- lib/savings_account.rb
+++ lib/savings_account.rb
@@ -1,13 +1,11 @@
-require 'pry'
 module Bank
-
-
   class SavingsAccount < Account
 
     def initialize(id, initial_balance, open_date)
       super
       @min_balance = 10.0
       @fee = 2.0
+      # Acct_type variable allows add_interest method from Account to function correctly for Savings Account
       @acct_type = "Savings"
 
       #Raises an argument error if the initial balance is less than 0
@@ -16,11 +14,14 @@ module Bank
       end
     end
 
-    # Rewrote the withdraw method in account so that only a super is needed - This line could come out but is being kept in for clarity.
+    # Savings Account uses the withdraw method from Account
+    # This line could come out but is being kept in for clarity.
     def withdraw(orig_amount_to_withdraw)
       super
     end
 
+    # Savings Account uses the add_interest method from Account
+    # This line could come out but is being kept in for clarity.
     def add_interest(rate)
       super
     end
