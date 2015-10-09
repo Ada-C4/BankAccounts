a46144f4ad4afb5a1935e2c87c9e91d16ef5bf94
Annalee <annaleeherrera@gmail.com>
2015-10-09 14:17:20 -0700

    Fixed check count on Checking Account file.

--- CheckingAccount.rb
+++ CheckingAccount.rb
@@ -4,6 +4,7 @@ module Bank
   class Checking < Account
     def initialize (name, initial_balance)
       super
+        @check_count = 3
 
     end
 
@@ -11,7 +12,7 @@ module Bank
       if withdrawal_amount > @balance
         puts "You have insufficient funds. You have $#{@balance} in your account."
       elsif withdrawal_amount > 0
-        @balance = @balance - (withdrawal_amount+1)
+        @balance = @balance - (withdrawal_amount + 100)
         puts "You withdrew $#{withdrawal_amount}. Your new balance is $#{@balance}."
         puts "You were also charged a $1 transaction fee."
       elsif withdrawal_amount <= @balance
@@ -21,18 +22,20 @@ module Bank
       return @balance
     end
 
-    def withdraw_using_check(check_amount, check_count=3)
-      check_count -= 1
-      @balance = @balance - check_amount
-      puts "You withdrew #{check_amount}. You may use #{check_count} checks to withdraw this month. Your balance is #{@balance}."
-      if @balance <= -10
+    def withdraw_using_check(check_amount)
+      if @balance - check_amount < -1000
         raise ArgumentError.new ("You cannot have an overdraft of more than $10.")
-      elsif check_count == 0
-        @balance = @balance - 2
-        puts "You have used your 3 free check uses this month.  You have been charged a $2 transaction fee."
-        check_count = 3
       end
-        return @balance
+
+      if @check_count > 0
+        @check_count -= 1
+        @balance = @balance - check_amount
+        puts "You withdrew #{check_amount}. You have #{@check_count} checks to use for withdrawal this month. Your balance is #{@balance}."
+      else
+        @balance = @balance - (check_amount + 200)
+        puts "You have used your 3 free check uses this month.  You have been charged a $2 transaction fee."
+      end
+      return @balance
     end
   end
 end
