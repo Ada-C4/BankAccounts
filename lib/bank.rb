6eada02e24db110104a89887dc615cf5eb42b11f
Tammy <misstamar@gmail.com>
2015-10-11 21:27:29 -0700

    Finish

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,28 @@
+module Bank
 require "./saving_account.rb"
 require "./chackingaccount.rb"
 require "./account.rb"
+require "./MoneyMarketAccount.rb"
+#
+# d = Bank::CheckingAccount.new(100000, 11-12-15)
+#
+# d.withdraw(500)
+#
+# p = Bank::SavingsAccount.new(123, 11-12-12)
+#
+# p.withdraw(1000)
+
+# r = Bank::MoneyMarketAccount.new(12, 12-12-12)
+#
+# r.withdraw(1000)
+#
+# r.withdraw(500)
+#
+# 10.times do
+#   (r.withdraw(200))
+# end
+
+
+
+
+end
