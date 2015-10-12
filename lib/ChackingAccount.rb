c93c8a409da1708a64c60451de2965f4254b7dd2
Tammy <misstamar@gmail.com>
2015-10-11 23:45:38 -0700

    finish

--- lib/ChackingAccount.rb
+++ lib/ChackingAccount.rb
@@ -1,27 +1,33 @@
 module Bank
+  require "./account.rb"
   class CheckingAccount < Account
 
+    attr_accessor :num_checks
 
-    def withdraw (amount)
-      super (amount, 1)
+    def initialize(id, date)
+      super
+      @num_checks = 0
     end
 
+    def withdraw(amount)
+      super(amount, 100, 0)
+    end
 
-    #   if amount + FEE > @balance
-    #     puts "You have $#{@balance} in your account. Wirg 4! fee"
-    #     puts "The maximun amount you can draw is #{@balance + 1} pennies"
-    #   else
-    #       puts "There is a $1 fee for each withdraw. Would you like to continue?"
-    #       answer = gets.chomp.downcase
-    #       if answer == y || answer == yes
-    #         puts "You had $#{@balance/100}"
-    #         @balance = @balance - amount - FEE
-    #         puts "The update account balance is $#{@balance/100}"
-    #       else
-    #         puts "Ok, Bye!"
-    #       end
-    #   end
-    #   return @balance
-    # end
-end
+    def withdraw_using_check(amount)
+        if @balace < (amount - 100) # $10 in cents
+          raise ArgumentError.new("The account can't go into overdraft up to -$10")
+        elsif @num_checks > 3
+          puts "Since you used more then 3 checks this month- you'll be charged with $2 fee"
+          @balance = @balance - 200 - amount
+          @num_checks += 1
+        else
+          @balance = @balance - amount
+        end
+        puts " Your current balance is $#{@balace/100}"
+    end
+
+    def reset_checks
+      @num_checks = 0
+    end
+  end
 end
--- lib/MoneyMarketAccount.rb
+++ lib/MoneyMarketAccount.rb
@@ -3,45 +3,61 @@ module Bank
 
   class MoneyMarketAccount < Account
 
-    def initialize (id, date)
+    attr_accessor :num_transactions
+
+    def initialize(id, date)
       super
       @num_transactions = 0
     end
 
-    def initial_balance (1000000)
-      super
+    def initial_balance
+      super(1000000)
     end
 
-    def withdraw (amount, 100, 1000000)
-      if check_transactions && check_balance
-        super(amount, 100, 1000000)
-        if show_balance < 1000000
-          puts "You can't do more transactions until the balance is decrease to $10,000"
-        end
+    def withdraw(amount)
+      if check_transactions && check_min_balance
+        puts "Your balance was $#{@balance/100}"
+        @balance -= amount
         @num_transactions +=1
+        puts "after you withdrew $#{amount/100}, your currecnt balance is $#{@balance/100}"
+        if @balance < 1000000
+          puts "You can't do more transactions until the balance will increase to $10,000"
+          puts "and you've been charged with $100 fee"
+          @balance -= 100
+        end
+      else
+        if !check_transactions
+          puts "Sorry you can't withdraw money since you've reached 6 transactions this month"
         else
-        puts "You can't withdraw money. Think why"
+          puts "you have $#{@balance/100} only in your account. Please deposit at least $#{1000000 - @balance}"
+          puts "It won't count as one of youe monthly transactions"
+        end
       end
     end
 
       #check if the costumer want to depit monet to reach the minimum balance
-    def reach_min_balance (amount)
-      if (show_balance < 1000000) && (amount > (1000000 - show_balance))
+    def reach_min_balance(amount)
+      if (@balance < 1000000) && (amount > (1000000 - @balance))
         return true
       else
         return false
       end
     end
 
+    def add_interest(rate)
+      super(rate)
+    end
 
+    def reset_transactions
+      @num_transactions = 0
+    end
 
-    def deposit (amount)
-      if check_transactions && check_balance ## check that the num of trasactions are less then 7 & the balance more then 10,000$
+    def deposit(amount)
+      if check_transactions && check_min_balance ## check that the num of trasactions are less then 7 & the balance more then 10,000$
         super
         @num_transactions += 1
-      else
-        if reach_min_balance(amount) #if he want to deposit money to reach $10,000 - don't count transactions
-          super(amout)
+      elsif reach_min_balance(amount) #if he want to deposit money to reach $10,000 - don't count transactions
+        super(amount)
       end
     end
 
@@ -53,17 +69,17 @@ module Bank
       end
     end
 
-    def check_balance
-      if show_balance < 1000000
+    def show_balance
+      puts "Your current balance is $#{@balance/100}"
+    end
+
+    def check_min_balance
+      if @balance < 1000000
         return false
       else
         return true
       end
     end
-
-
-
-
 
 
   end
