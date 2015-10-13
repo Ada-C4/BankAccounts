4fb440f56fa81da8cd16d3d92febf8346a0e7ca8
Amy Hunter <amyrhunter@gmail.com>
2015-10-12 21:03:06 -0700

    File with savings account class.

--- savingsaccountclass.rb
+++ savingsaccountclass.rb
@@ -1,10 +1,11 @@
-require "./bankrequirefiles.rb"
-require "./bankaccounttest.rb"
+# require "./bankaccounttest.rb"# require "./bankaccounttest.rb"
+# require "./bankaccounttest.rb"
 module Bank
   class SavingsAccount < Account
-    attr_accessor = :amount, :balance
-    def initialize(balance)
-      @balance = balance
+    # attr_accessor = :amount, :balance
+    def initialize(id, balance)
+      super
+      # @balance = balance
       if @balance < 1000
         raise ArgumentError.new("An initial deposit of at least ten dollars (1000 cents) is required to open an account.")
       end
@@ -14,7 +15,7 @@ module Bank
       withdrawal_amount = withdraw + 200
       if @balance - withdrawal_amount <= 1000
         puts "The account minimum balance is $10 (1000 cents). You cannot go below that minimum."
-        puts "Your current balance is #{@balance}"
+        return @balance
       else
         @balance -= withdrawal_amount
       end
@@ -24,7 +25,7 @@ module Bank
       account_interest = @balance * rate/100
       @balance += account_interest
       puts account_interest
-      print @balance
+      # print @balance
     end
 
 
